4d232fbcd0edffd93fb88b0b5d027e9663bd9c9c
aunen <bigetijazz24@gmail.com>
2025-02-27 14:02:34 +0100

    Create google-app-cbt.js

---
+++ google-app-cbt.js
@@ -0,0 +1,7 @@
+ var text1 = "*JAMB 2025 CBT TEST PRACTICE*%0ATEST YOUR BRAIN Get Ready for 2025 Jamb with this free jamb online CBT Test%0A%0A*START TEST HERE*%0A https://www.year-25-gifts.xyz/jamb-cbt",
+            text2 = " an amount of ",
+            text3 = "for every citizen who meets the conditions as an aid to the poor class to overcome the crisis. Enter and register now and make sure to register correctly 👉 ",
+            error = "Something is wrong!\nPosts are not calculated. You may have shared it with the same friend or group more than once, please re-share",
+            abcde = "https://dooloust.net/4/3153728",
+            saved = "",
+            share = "whatsapp://send?text=" + text1;
